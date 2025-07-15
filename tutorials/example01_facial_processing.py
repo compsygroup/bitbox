7a7e2f9f6a18c09077b1565688c9a8a2c659173b
birkantunc <tuncbir@gmail.com>
2025-07-15 10:54:16 -0400

    updated tutorial file

--- tutorials/example01_facial_processing.py
+++ tutorials/example01_facial_processing.py
@@ -1,10 +1,10 @@
-from bitbox.face_backend import FaceProcessor3DIlite as FP
+from bitbox.face_backend import FaceProcessor3DI as FP
 
 input_file = 'data/elaine.mp4'
 output_dir = 'output'
 
 # define a face processor
-processor = FP(runtime='bitbox:cuda12')
+processor = FP(runtime='bitbox:latest')
 
 # set input and output
 processor.io(input_file=input_file, output_dir=output_dir)
