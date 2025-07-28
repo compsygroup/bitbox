68e540c5f628979dbf6ab9f688590cd90f2d58da
Gokul N <nairg1@chop.edu>
2025-07-28 10:59:31 -0400

    tutorial example update

--- tutorials/example06_remote_slurm_execution.py
+++ tutorials/example06_remote_slurm_execution.py
@@ -1,5 +1,5 @@
 from bitbox.face_backend import FaceProcessor3DI as FP
-from bitbox.utilities.slurm import slurm_submit
+from bitbox.utilities.slurm import slurm_submit, slurm_status
 
 input_file = 'IS001_parent.mp4'
 output_dir = 'output'
@@ -8,9 +8,15 @@ slurm_config = {
     'host': 'abc.xyz.edu',   # your login node
     'job_name': 'bitbox_process_test',
     'remote_input_dir': 'hpc/data/path_to_data_directory/',
-    'remote_output_dir': 'hpc/data/path_to_output_directory/'
+    'remote_output_dir': 'hpc/data/path_to_output_directory/',
+    'venv_path': '/hpc/mnt/path/to/bitbox_venv',
+    'runtime': 'bitbox:latest', # docker image or singularity image path or 3di runtime path
+    'parameters': {'fast': True, 'camera_model': 30}, # parameters for FaceProcessor3DI
 }
-processor = FP(fast=True, verbose=True, slurm=True) 
 
 # submits slurm job to server and return jobID
-jobID = slurm_submit(processor, slurm_config, input_file=input_file, output_dir=output_dir,runtime='bitbox:latest') 
+jobID = slurm_submit(FP, slurm_config, input_file=input_file, output_dir=output_dir) 
+
+# check job status by ID
+slurm_status(jobID, slurm_config)  
+
