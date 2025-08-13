955748e35a213f3e1e49bf81ac354a2f178e53a5
Gokul N <nairg1@chop.edu>
2025-08-13 12:06:29 -0400

    Remove plot_landmarks import from visualization utilities

--- src/bitbox/utilities/__init__.py
+++ src/bitbox/utilities/__init__.py
@@ -2,5 +2,5 @@ from .caching import FileCache, generate_file_hash
 from .file_types import get_data_values
 from .landmarks import landmark_to_feature_mapper
 from .system import select_gpu, detect_container_type
-from .visualization import plot, plot_rects, plot_landmarks
+from .visualization import plot, plot_rects
 from .slurm import SlurmClient
