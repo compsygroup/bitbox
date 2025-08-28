f84a4cbd2e8446cbae93989a1d443e7d1e763e75
Gokul N <nairg1@chop.edu>
2025-08-28 11:59:43 -0400

    undo tutorial change

--- tutorials/example01_facial_processing.py
+++ tutorials/example01_facial_processing.py
@@ -1,18 +1,23 @@
-from bitbox.face_backend import FaceProcessor3DIlite as FP
+from bitbox.face_backend import FaceProcessor3DI as FP
 
-input_file = '/home/nairg1/Documents/bitbox/bitbox/tutorials/data/elaine.mp4'
-output_dir = '/home/nairg1/Documents/bitbox/bitbox/tutorials/output'
-
+# define input file and output directory
+input_file = 'data/elaine.mp4'
+output_dir = 'output'
 
 # define a face processor
-processor = FP(runtime='bitbox:latest')
+processor = FP()
 
 # set input and output
 processor.io(input_file=input_file, output_dir=output_dir)
 
-rect, land, exp_global, pose, land_can, exp_local = processor.run_all(normalize=True)
+# detect faces
+rects = processor.detect_faces()
 
-processor.plot(exp_global,overlay=[rect,land],video=True) # plot landmarks with rectangle overlay and pose
-# processor.plot(rect,overlay=land,video=True) # plot rectangle with landmarks overlay and pose
-# processor.plot(pose,overlay=[land,rect],video=True) # plot canonical landmarks with rectangle and landmarks overlay and pose
-# processor.plot(exp_local,overlay=[rect,land],video=True)
+# detect landmarks
+lands = processor.detect_landmarks()
+
+# compute global expressions
+exp_global, pose, lands_can = processor.fit()
+
+# compute localized expressions
+exp_local = processor.localized_expressions()
